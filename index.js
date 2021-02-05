36a10f7065bae3e85af854fed128a17fb40f4b71
Abbassihraf <hraf.abbassi@gmail.com>
2021-02-05 15:55:05 +0100

    Install electron-reloader

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
 const electron = require ('electron');
 const url = require ('url');
 const path = require ('path');
+try {
+    require('electron-reloader')(module)
+  } catch (_) {}
 
 
 const {app, BrowserWindow}= electron;
 
-let mainWndow;
-
-
-// Liste for app to be ready
-app.on('ready', function(){
-    // Create new window
-    mainWndow = new BrowserWindow({});
-    // Load html into window
-
-});
+function createWindow() {
+    // Create the browser window.
+    const win = new BrowserWindow({
+      width: 800,
+      height: 600,
+      webPreferences: {
+        nodeIntegration: true,
+      },
+    })
+  
+    // and load the index.html of the app.
+    win.loadFile('mainWindow.html')
+  }
+  
+  app.whenReady().then(createWindow)
