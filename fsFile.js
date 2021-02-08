6661036c35bc730751df30c45239c960f47d3726
Abbassihraf <hraf.abbassi@gmail.com>
2021-02-08 16:50:38 +0100

    Function to delete temporary files.

--- fsFile.js
+++ fsFile.js
@@ -56,7 +56,99 @@ const analyse = () => {
 };
 
 
+const deleted = () => {
+  if (i == 0) {
+    i = 1;
+    let elem = document.getElementById("myBar");
+    document.getElementById("myProgress").style.display = "block";
+    document.getElementById("btn").style.display = "none";
+    document.getElementById("icon").style.display = "none";
 
+    let width = 1;
+    let id = setInterval(frame, 20);
+
+    function frame() {
+      if (width >= 100) {
+        // -----------------------------------
+        fs.readdir(directory, (err, files) => {
+          if (err) throw err;
+          if (files.length === 0) {
+            document.getElementById("txt").innerHTML = "No file found!";
+            document.getElementById("btn2").style.display = "block";
+            document.getElementById("icon").style.display = "block";
+            return;
+          } else {
+            for (const file of files) {
+              document.getElementById("txt").innerHTML =
+                files.length + " files deleted!";
+              rimraf(path.join(directory, file), (err) => {
+                if (err) throw err;
+                document.getElementById("btn2").style.display = "block";
+                document.getElementById("icon").style.display = "block";
+                return;
+              });
+            }
+          }
+        });
+
+        // -----------------------------------
+        clearInterval(id);
+        i = 0;
+        elem.style.display = "none";
+      } else {
+        width++;
+        elem.style.width = width + "%";
+      }
+    }
+  }
+};
+
+const nettoyer = () => {
+  if (i == 0) {
+    i = 1;
+    let elem = document.getElementById("myBar");
+    document.getElementById("myProgress").style.display = "block";
+    document.getElementById("btn").style.display = "none";
+    document.getElementById("icon").style.display = "none";
+
+    let width = 1;
+    let id = setInterval(frame, 20);
+
+    function frame() {
+      if (width >= 100) {
+        // -----------------------------------
+        fs.readdir(directory, (err, files) => {
+          if (err) throw err;
+          if (files.length === 0) {
+            document.getElementById("txt").innerHTML = "No file found!";
+            document.getElementById("btn2").style.display = "block";
+            document.getElementById("icon").style.display = "block";
+            return;
+          } else {
+            for (const file of files) {
+              document.getElementById("txt").innerHTML =
+                files.length + " files deleted!";
+              rimraf(path.join(directory, file), (err) => {
+                if (err) throw err;
+                document.getElementById("btn2").style.display = "block";
+                document.getElementById("icon").style.display = "block";
+                return;
+              });
+            }
+          }
+        });
+
+        // -----------------------------------
+        clearInterval(id);
+        i = 0;
+        elem.style.display = "none";
+      } else {
+        width++;
+        elem.style.width = width + "%";
+      }
+    }
+  }
+};
 
 const home = () =>{
   location.reload(); 
